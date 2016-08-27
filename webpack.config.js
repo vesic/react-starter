7d39bb99dff63bb1dd20113abb3557e4fac9fde1
dusan.vesic <dusan.vesic@gmail.com>
2016-08-27 12:22:52 +0200

    Add basic config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: "style!css"
+    },{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }]
+  }
+};
